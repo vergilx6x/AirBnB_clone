974131ccbf4ce6dc68ee19843e3602f4e9fc88fa
vergilx6x <med.work96@hotmail.com>
2024-05-18 05:26:29 -0700

    more_classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
     This module contains our BaseModel class
 """
---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represent a User class.
+
+    Attributes:
+        email : The email of the user.
+        password : The password of the user.
+        first_name : The first name of the user.
+        last_name : The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
